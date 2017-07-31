a44e6f44d4809aa5771964512feb0e51485fcecb
onlineecare <30410703+onlineecare@users.noreply.github.com>
2017-07-31 19:48:46 +0200

    Update updatePodsImage.sh

--- updatePodsImage.sh
+++ updatePodsImage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Updating deployment image for frontend-test"
-~/.kube/kubectl set image deployment/frontend-test frontend-test=336627915605.dkr.ecr.eu-central-1.amazonaws.com/frontend-test:latest
+echo "~/.kube/kubectl set image deployment/${KUBERNETES_SERVICE_NAME} ${KUBERNETES_SERVICE_NAME}=${DOCKER_REGISTRY_URL}/${KUBERNETES_SERVICE_NAME}:build-${CIRCLE_SHA1}"
+~/.kube/kubectl set image deployment/${KUBERNETES_SERVICE_NAME} ${KUBERNETES_SERVICE_NAME}=${DOCKER_REGISTRY_URL}/${KUBERNETES_SERVICE_NAME}:build-${CIRCLE_SHA1}
 echo "Finished Updating deployment image for frontend-test!!!"
